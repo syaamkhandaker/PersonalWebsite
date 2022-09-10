f89475c6ed833d490db68370485853a45b2a65e3
Syaam Khandaker <70422626+syaamkhandaker@users.noreply.github.com>
2022-09-10 16:28:32 -0400

    Add files via upload

--- src/components/Contact Me/ContactMe.jsx
+++ src/components/Contact Me/ContactMe.jsx
@@ -6,7 +6,7 @@ function ContactMe () {
         <div className='resume__view-contact'>
             <div className='resume__view-contact-text'>
                 <h1><a id="Contact_Me">Any Questions? Feel free to contact me.</a></h1>
-               <h1 className='resume__view-contact-text-border'><a href={`mailto:syaamkhandaker@gmail.com`}>Contact Me</a></h1> 
+               <h1 className='resume__view-contact-text-border'><a href="mailto:syaamkhandaker@gmail.com" rel="noopener noreferrer" target="_blank">Contact Me</a></h1> 
             </div>
         </div>
     );
